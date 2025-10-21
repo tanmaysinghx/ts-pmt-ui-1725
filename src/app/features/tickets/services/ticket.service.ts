d5ae70b91489ef500186dff6c351e2738bdc523d
Tanmay Singh <tanmaysinghx@gmail.com>
2025-10-21 18:19:59 +0530

    Imprvemnnt

--- src/app/features/tickets/services/ticket.service.ts
+++ src/app/features/tickets/services/ticket.service.ts
@@ -11,7 +11,7 @@ export class TicketService {
   constructor(private readonly http: HttpClient) { }
 
   getCMSDataCreateTicket(): Observable<any> {
-    let dataUrl: string = environment.cmsUrl + 'src/assets/data/create-ticket-form-data.json'; 
+    let dataUrl: string = environment.cmsUrl + 'assets/data/create-ticket-form-data.json'; 
     return this.http.get<any>(dataUrl);
   }
 }
